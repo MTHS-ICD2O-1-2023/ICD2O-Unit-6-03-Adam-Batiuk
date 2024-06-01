8bf86bb3e38f22efbcb83673fad4280f4499aa49
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-06-01 23:46:09 +0000

    done

--- js/script.js
+++ js/script.js
@@ -6,17 +6,37 @@
 
 "use strict"
 
-function updateCookieCount() {
-  if (localStorage.getItem("counter") === null) {
-    localStorage.setItem("counter", 0)
-  }
-  let clicks = localStorage.getItem("counter")
-  document.getElementById("answer").innerHTML = clicks
+// convert K to C to one decimal
+function kelvinToCelcius(tempK) {
+  const tempC = tempK - 273.15
+  return tempC.toFixed(1)
 }
 
-function cookieClicked() {
-  let clicks = parseInt(localStorage.getItem("counter")) + 1
-  localStorage.setItem("counter", clicks)
-  // print answer
-  document.getElementById("answer").innerHTML = clicks
+async function getWeather() {
+  try {
+    const resultJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
+    const jsonData = await resultJSON.json()
+    //console.log(jsonData)
+    // extract data
+    const iconId = jsonData.weather[0].icon
+    const description = jsonData.weather[0].description
+    const temp = kelvinToCelcius(jsonData.main.temp)
+    const feelsLike = kelvinToCelcius(jsonData.main.feels_like)
+    const tempMin = kelvinToCelcius(jsonData.main.temp_min)
+    const tempMax = kelvinToCelcius(jsonData.main.temp_max)
+    const pressure = jsonData.main.pressure
+    const humidity = jsonData.main.humidity
+    // display data
+    document.getElementById("icon").src = "https://openweathermap.org/img/wn/" + iconId + "@2x.png"
+    document.getElementById("icon").alt = description
+    document.getElementById("description").innerHTML = description
+    document.getElementById("temp").innerHTML = "Temp: " + temp + " °C"
+    document.getElementById("feels-like").innerHTML = "Feels like: " + feelsLike + " °C"
+    document.getElementById("temp-min").innerHTML = "Min: " + tempMin + " °C"
+    document.getElementById("temp-max").innerHTML = "Max: " + tempMax + " °C"
+    document.getElementById("pressure").innerHTML = "Pressure: " + pressure + " mb"
+    document.getElementById("humidity").innerHTML = "Humidity: " + humidity + "%"
+  } catch (error) {
+    console.error(error)
+  }
 }
