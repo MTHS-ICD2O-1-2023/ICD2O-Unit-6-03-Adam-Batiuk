e01b114a8e6fe10bc00caba52d9a5ab416cfdd19
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-06-01 22:20:50 +0000

    start

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Adam Batiuk All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adam Batiuk
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function updateCookieCount() {
+  if (localStorage.getItem("counter") === null) {
+    localStorage.setItem("counter", 0)
+  }
+  let clicks = localStorage.getItem("counter")
+  document.getElementById("answer").innerHTML = clicks
+}
+
+function cookieClicked() {
+  let clicks = parseInt(localStorage.getItem("counter")) + 1
+  localStorage.setItem("counter", clicks)
+  // print answer
+  document.getElementById("answer").innerHTML = clicks
+}
